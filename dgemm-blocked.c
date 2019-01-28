efa17810e090dd01ca8e62972e4ef8e2904060d5
xxycrab <xiexinyucrab@126.com>
2019-01-28 15:44:33 -0800

    Update dgemm-blocked.c
    3-level blocking: done
    vectorization (register): done
    vector tiling: done
    loop unrolling: done
    
    problems: 
    1. padding matrix in case of edge problems by constructing new matrix. But failing to free old matrix results in memory overflow.
    2. in vector tiling, can only load B one row a time, otherwise cause computation error. There should be some way to fix it which then can further improve performance by about 1Gflop/s

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -10,13 +10,19 @@ const char *dgemm_desc = "Simple blocked dgemm.";
 
 #include <immintrin.h>
 #include <avx2intrin.h>
+#include "debugMat.h"
+#include "debugMat.c"
 
 #if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 1024
-#define BLOCK_SIZE_2 512
-#define BLOCK_SIZE_3_M 64
-#define BLOCK_SIZE_3_N 64
-#define BLOCK_SIZE_3_K 64
+#define BLOCK_SIZE_M 512
+#define BLOCK_SIZE_1_M 256
+#define BLOCK_SIZE_2_M 32
+#define BLOCK_SIZE_N 512
+#define BLOCK_SIZE_1_N 256
+#define BLOCK_SIZE_2_N 32
+#define BLOCK_SIZE_K 512
+#define BLOCK_SIZE_1_K 256
+#define BLOCK_SIZE_2_K 32
 // #define BLOCK_SIZE 719
 #endif
 
@@ -25,82 +31,79 @@ const char *dgemm_desc = "Simple blocked dgemm.";
 /* This auxiliary subroutine performs a smaller dgemm operation
  *  C := C + A * B
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
+
+/* Vector tiling and loop unrolling */
 static void do_block(int lda, int M, int N, int K, double *A, double *B, double *C) {
     /* For each row i of A */
-    for (int i = 0; i < M / 6; ++i) {
+    for (int i = 0; i < M / 4; ++i) {
         /* For each column j of B */
         for (int j = 0; j < N / 4; ++j) {
-            register __m256d c_00_03 = _mm256_loadu_pd(C + 6 * i * lda + j * 4);
-            register __m256d c_10_13 = _mm256_loadu_pd(C + (6 * i + 1) * lda + j * 4);
-            register __m256d c_20_23 = _mm256_loadu_pd(C + (6 * i + 2) * lda + j * 4);
-            register __m256d c_30_33 = _mm256_loadu_pd(C + (6 * i + 3) * lda + j * 4);
-            register __m256d c_40_43 = _mm256_loadu_pd(C + (6 * i + 4) * lda + j * 4);
-            register __m256d c_50_53 = _mm256_loadu_pd(C + (6 * i + 5) * lda + j * 4);
+            register __m256d c_00_03 = _mm256_loadu_pd(C + min(4 * i, M - 1) * lda + j * 4);
+            register __m256d c_10_13 = _mm256_loadu_pd(C + min(4 * i + 1, M - 1) * lda + j * 4);
+            register __m256d c_20_23 = _mm256_loadu_pd(C + min(4 * i + 2, M - 1) * lda + j * 4);
+            register __m256d c_30_33 = _mm256_loadu_pd(C + min(4 * i + 3, M - 1) * lda + j * 4);
 
-            for (int kk = 0; kk < 4 && kk < K - j * 4; kk++) {
-                register __m256d a0x = _mm256_broadcast_sd(A + 6 * i * lda + j * 4 + kk);
-                register __m256d a1x = _mm256_broadcast_sd(A + (6 * i + 1) * lda + j * 4 + kk);
-                register __m256d a2x = _mm256_broadcast_sd(A + (6 * i + 2) * lda + j * 4 + kk);
-                register __m256d a3x = _mm256_broadcast_sd(A + (6 * i + 3) * lda + j * 4 + kk);
-                register __m256d a4x = _mm256_broadcast_sd(A + (6 * i + 4) * lda + j * 4 + kk);
-                register __m256d a5x = _mm256_broadcast_sd(A + (6 * i + 5) * lda + j * 4 + kk);
-                for (int k = 0; k < K / 4; k++) {
-                    register __m256d b_00_03 = _mm256_loadu_pd(B + 4 * k * lda + j * 4);
-                    register __m256d b_10_13 = _mm256_loadu_pd(B + (4 * k + 1) * lda + j * 4);
-                    register __m256d b_20_23 = _mm256_loadu_pd(B + (4 * k + 2) * lda + j * 4);
-                    register __m256d b_30_33 = _mm256_loadu_pd(B + (4 * k + 3) * lda + j * 4);
+            /* Loop unrolling */
+            for (int k = 0; k < K; k += 1) {
+                register __m256d b_00_03 = _mm256_loadu_pd(B + k * lda + j * 4);
+//                register __m256d b_10_13 = _mm256_loadu_pd(B + (k + 1) * lda + j * 4);
 
-                    c_00_03 = _mm256_fmadd_pd(a0x, b_00_03, c_00_03);
-                    c_00_03 = _mm256_fmadd_pd(a0x, b_10_13, c_00_03);
-                    c_00_03 = _mm256_fmadd_pd(a0x, b_20_23, c_00_03);
-                    c_00_03 = _mm256_fmadd_pd(a0x, b_30_33, c_00_03);
+                register __m256d a00 = _mm256_broadcast_sd(A + (4 * i) * lda + k);
+                register __m256d a10 = _mm256_broadcast_sd(A + (4 * i + 1) * lda + k);
+                register __m256d a20 = _mm256_broadcast_sd(A + (4 * i + 2) * lda + k);
+                register __m256d a30 = _mm256_broadcast_sd(A + (4 * i + 3) * lda + k);
+//                register __m256d a01 = _mm256_broadcast_sd(A + (4 * i) * lda + k + 1);
+//                register __m256d a11 = _mm256_broadcast_sd(A + (4 * i + 1) * lda + k + 1);
+//                register __m256d a21 = _mm256_broadcast_sd(A + (4 * i + 1) * lda + k + 1);
+//                register __m256d a31 = _mm256_broadcast_sd(A + (4 * i + 1) * lda + k + 1);
 
-                    c_10_13 = _mm256_fmadd_pd(a1x, b_00_03, c_10_13);
-                    c_10_13 = _mm256_fmadd_pd(a1x, b_10_13, c_10_13);
-                    c_10_13 = _mm256_fmadd_pd(a1x, b_20_23, c_10_13);
-                    c_10_13 = _mm256_fmadd_pd(a1x, b_30_33, c_10_13);
-
-                    c_20_23 = _mm256_fmadd_pd(a2x, b_00_03, c_20_23);
-                    c_20_23 = _mm256_fmadd_pd(a2x, b_10_13, c_20_23);
-                    c_20_23 = _mm256_fmadd_pd(a2x, b_20_23, c_20_23);
-                    c_20_23 = _mm256_fmadd_pd(a2x, b_30_33, c_20_23);
-
-                    c_30_33 = _mm256_fmadd_pd(a3x, b_00_03, c_30_33);
-                    c_30_33 = _mm256_fmadd_pd(a3x, b_10_13, c_30_33);
-                    c_30_33 = _mm256_fmadd_pd(a3x, b_20_23, c_30_33);
-                    c_30_33 = _mm256_fmadd_pd(a3x, b_30_33, c_30_33);
-
-                    c_40_43 = _mm256_fmadd_pd(a4x, b_00_03, c_40_43);
-                    c_40_43 = _mm256_fmadd_pd(a4x, b_10_13, c_40_43);
-                    c_40_43 = _mm256_fmadd_pd(a4x, b_20_23, c_40_43);
-                    c_40_43 = _mm256_fmadd_pd(a4x, b_30_33, c_40_43);
-
-                    c_50_53 = _mm256_fmadd_pd(a5x, b_00_03, c_50_53);
-                    c_50_53 = _mm256_fmadd_pd(a5x, b_10_13, c_50_53);
-                    c_50_53 = _mm256_fmadd_pd(a5x, b_20_23, c_50_53);
-                    c_50_53 = _mm256_fmadd_pd(a5x, b_30_33, c_50_53);
-                }
+                c_00_03 = _mm256_fmadd_pd(a00, b_00_03, c_00_03);
+                c_10_13 = _mm256_fmadd_pd(a10, b_00_03, c_10_13);
+                c_20_23 = _mm256_fmadd_pd(a20, b_00_03, c_20_23);
+                c_30_33 = _mm256_fmadd_pd(a30, b_00_03, c_30_33);
+//                if(k+1<K) {
+//                    c_00_03 = _mm256_fmadd_pd(a01, b_10_13, c_00_03);
+//                    c_10_13 = _mm256_fmadd_pd(a11, b_10_13, c_10_13);
+//                    c_20_23 = _mm256_fmadd_pd(a21, b_10_13, c_20_23);
+//                    c_30_33 = _mm256_fmadd_pd(a31, b_10_13, c_30_33);
+//                }
             }
-            _mm256_storeu_pd(C + 6 * i * lda + j * 4, c_00_03);
-            _mm256_storeu_pd(C + (6 * i + 1) * lda + j * 4, c_10_13);
-            _mm256_storeu_pd(C + (6 * i + 2) * lda + j * 4, c_20_23);
-            _mm256_storeu_pd(C + (6 * i + 3) * lda + j * 4, c_30_33);
-            _mm256_storeu_pd(C + (6 * i + 4) * lda + j * 4, c_40_43);
-            _mm256_storeu_pd(C + (6 * i + 5) * lda + j * 4, c_50_53);
+            _mm256_storeu_pd(C + (4 * i) * lda + j * 4, c_00_03);
+            _mm256_storeu_pd(C + (4 * i + 1) * lda + j * 4, c_10_13);
+            _mm256_storeu_pd(C + (4 * i + 2) * lda + j * 4, c_20_23);
+            _mm256_storeu_pd(C + (4 * i + 3) * lda + j * 4, c_30_33);
         }
-
-//            /* Compute C(i,j) */
-//            double cij = C[i * lda + j];
-//            for (int k = 0; k < K; ++k)
-//#ifdef TRANSPOSE
-//                cij += A[i*lda+k] * B[j*lda+k];
-//#else
-//                    cij += A[i * lda + k] * B[k * lda + j];
-//#endif
-//            C[i * lda + j] = cij;
     }
 }
 
+/* Third level blocking, L2 cache*/
+static void do_block_L2(int lda, int M1, int N1, int K1, double *A, double *B, double *C) {
+    for (int i = 0; i < M1; i += BLOCK_SIZE_2_M) {
+        for (int j = 0; j < N1; j += BLOCK_SIZE_2_N) {
+            for (int k = 0; k < K1; k += BLOCK_SIZE_2_K) {
+                int M2 = min(BLOCK_SIZE_2_M, M1 - i);
+                int N2 = min (BLOCK_SIZE_2_N, N1 - j);
+                int K2 = min (BLOCK_SIZE_2_K, K1 - k);
+                do_block(lda, M2, N2, K2, A + i * lda + k, B + k * lda + j, C + i * lda + j);
+            }
+        }
+    }
+}
+
+/* Second level blocking, L1 cache*/
+static void do_block_L1(int lda, int M, int N, int K, double *A, double *B, double *C) {
+    for (int i = 0; i < M; i += BLOCK_SIZE_1_M) {
+        for (int j = 0; j < N; j += BLOCK_SIZE_1_N) {
+            for (int k = 0; k < K; k += BLOCK_SIZE_1_K) {
+                int M1 = min(BLOCK_SIZE_1_M, M - i);
+                int N1 = min (BLOCK_SIZE_1_N, N - j);
+                int K1 = min (BLOCK_SIZE_1_K, K - k);
+
+                do_block_L2(lda, M1, N1, K1, A + i * lda + k, B + k * lda + j, C + i * lda + j);
+            }
+        }
+    }
+}
 
 /* This routine performs a dgemm operation
  *  C := C + A * B
@@ -115,31 +118,56 @@ void square_dgemm(int lda, double *A, double *B, double *C) {
         B[j*lda+i] = t;
     }
 #endif
-    /* For each block-row of A */
-    for (int i = 0; i < lda; i += BLOCK_SIZE)
-        /* For each block-column of B */
-        for (int j = 0; j < lda; j += BLOCK_SIZE)
-            /* Accumulate block dgemms into block of C */
-            for (int k = 0; k < lda; k += BLOCK_SIZE)
-                for (int ii = i; ii < i + BLOCK_SIZE && ii < lda; ii += BLOCK_SIZE_2)
-                    for (int jj = j; jj < j + BLOCK_SIZE && jj < lda; jj += BLOCK_SIZE_2)
-                        for (int kk = k; kk < k + BLOCK_SIZE && kk < lda; kk += BLOCK_SIZE_2)
-                            for (int iii = ii; iii < ii + BLOCK_SIZE_2 && iii < lda; iii += BLOCK_SIZE_3_M)
-                                for (int jjj = jj; jjj < jj + BLOCK_SIZE_2 && jjj < lda; jjj += BLOCK_SIZE_3_N)
-                                    for (int kkk = kk; kkk < kk + BLOCK_SIZE_2 && kkk < lda; kkk += BLOCK_SIZE_3_K) {
-                                        /* Correct block dimensions if block "goes off edge of" the matrix */
-                                        int M = min (BLOCK_SIZE_3_M, lda - iii);
-                                        int N = min (BLOCK_SIZE_3_N, lda - jjj);
-                                        int K = min (BLOCK_SIZE_3_K, lda - kkk);
+    /* Deal with edge condition
+     * padding matrix to 4n*4n */
+    int r = lda % 4, LDA = lda;   //residual of 4*4 tiling
+    if (r != 0) {
+        LDA = lda + (4 - r);
+    }
 
-                                        /* Perform individual block dgemm */
+    double *A_new = (double *) _mm_malloc(LDA * LDA * sizeof(double), 32);
+    double *B_new = (double *) _mm_malloc(LDA * LDA * sizeof(double), 32);
+    double *C_new = (double *) _mm_malloc(LDA * LDA * sizeof(double), 32);
+
+    for (int i = 0; i < LDA; i++) {
+        for (int j = 0; j < LDA; j++) {
+            if (i < lda && j < lda) {
+                A_new[i * LDA + j] = A[i * lda + j];
+                B_new[i * LDA + j] = B[i * lda + j];
+                C_new[i * LDA + j] = C[i * lda + j];
+            } else {
+                A_new[i * LDA + j] = 0.;
+                B_new[i * LDA + j] = 0.;
+                C_new[i * LDA + j] = 0.;
+            }
+        }
+    }
+//    free(A);
+//    free(B);
+
+    /* For each block-row of A */
+    for (int i = 0; i < LDA; i += BLOCK_SIZE_M)
+        /* For each block-column of B */
+        for (int j = 0; j < LDA; j += BLOCK_SIZE_N)
+            /* Accumulate block dgemms into block of C */
+            for (int k = 0; k < LDA; k += BLOCK_SIZE_K) {
+                /* Correct block dimensions if block "goes off edge of" the matrix */
+                int M = min (BLOCK_SIZE_M, LDA - i);
+                int N = min (BLOCK_SIZE_N, LDA - j);
+                int K = min (BLOCK_SIZE_K, LDA - k);
+
+                /* Perform individual block dgemm */
 #ifdef TRANSPOSE
-                                        do_block(lda, M, N, K, A + iii*lda + kkk, B + jjj*lda + kkk, C + iii*lda + jjj);
+                do_block_L1(LDA, M, N, K, A_new + i*LDA + k, B_new + j*LDA + k, C_new + i*LDA + j);
 #else
-                                        do_block(lda, M, N, K, A + iii * lda + kkk, B + kkk * lda + jjj,
-                                                 C + iii * lda + jjj);
+                do_block_L1(LDA, M, N, K, A_new + i * LDA + k, B_new + k * LDA + j, C_new + i * LDA + j);
+                for(int i = 0; i<lda; i++){
+                    for(int j = 0; j<lda; j++){
+                        C[i*lda+j] = C_new[i*LDA+j];
+                    }
+                }
 #endif
-                                    }
+            }
 #if TRANSPOSE
     for (int i = 0; i < lda; ++i)
     for (int j = i+1; j < lda; ++j) {
